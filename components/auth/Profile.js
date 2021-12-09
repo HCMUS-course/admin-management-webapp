0c6b5a714045090f20122fdbbeb4ada8a3ab2eb9
19127324 <64062447+19127324@users.noreply.github.com>
2021-12-09 11:29:56 +0700

    update edit profile

--- components/auth/Profile.js
+++ components/auth/Profile.js
@@ -6,14 +6,13 @@ const User = mongoose.model('user');
 
 
 router.get('/:id',(req,res)=>{
- 
-
   User.findById(req.params.id,(err,doc)=>{
          
           res.render('adminProfile',{
             viewTitle:"Profile", 
             user : doc,
-            btnType : "hidden"
+            btnType : "hidden",
+            stateEdit:""
           });
        
   });
@@ -25,11 +24,8 @@ router.post('/edit/:id',(req,res) =>{
   
   
 });
-router.post('/',(req,res)=>{
-  
-    res.redirect("/");
-  
-});
+
+
 function updateRecord(req, res) {
   User.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, doc) => {
       if (!err) { 
@@ -47,7 +43,8 @@ router.get('/edit/:id',(req,res)=>{
           res.render('adminProfile',{
             viewTitle:"Edit Profile",
             user : doc,
-            btnType : ""
+            btnType : "",
+            stateEdit : "hidden"
           });
         
       
