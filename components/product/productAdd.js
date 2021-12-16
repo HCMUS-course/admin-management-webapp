eb955399792a305e968b186d539974810d27260c
19127324 <64062447+19127324@users.noreply.github.com>
2021-12-16 20:47:49 +0700

    update edit picture

--- components/product/productAdd.js
+++ components/product/productAdd.js
@@ -21,18 +21,34 @@ router.post('/', imageUpload, async(req,res)=>{
   var pic3 = "";
   var pic4 = "";
   const fd = "product/" + req.body.type + "/" +req.body.name; 
-  try{
-    pic1 = await cloudinary.uploader.upload(req.files.image1[0].path,{folder : fd,public_id: "p1"});
-    pic2 = await cloudinary.uploader.upload(req.files.image2[0].path,{folder : fd,public_id: "p2"});
-    pic3 = await cloudinary.uploader.upload(req.files.image3[0].path,{folder : fd,public_id: "p3"});
-    pic4 = await cloudinary.uploader.upload(req.files.image4[0].path,{folder : fd,public_id: "p4"});
+  
+  for (var i in req.files){
+    if(req.files[i][0].fieldname == "image1"){
+     
+      pic1 = await cloudinary.uploader.upload(req.files.image1[0].path,{folder : fd,public_id: "p1"});
+      
+    }
+    else if (req.files[i][0].fieldname == "image2"){
+      
+      pic2 = await cloudinary.uploader.upload(req.files.image2[0].path,{folder : fd,public_id: "p2"});
+      
+    }
+    else if (req.files[i][0].fieldname == "image3"){
+     
+      pic3 = await cloudinary.uploader.upload(req.files.image3[0].path,{folder : fd,public_id: "p3"});
+      
+    }
+    else if (req.files[i][0].fieldname == "image4")
+    {
+     
+      pic4 = await cloudinary.uploader.upload(req.files.image4[0].path,{folder : fd,public_id: "p4"});
     
+    }
   }
-  catch(err){
-    console.log(err);
-  }
-
-
+ 
+ 
+ 
+  
   var newProduct = new Product();
     newProduct.productType = req.body.type;
     newProduct.name = req.body.name;
@@ -85,47 +101,157 @@ router.post('/', imageUpload, async(req,res)=>{
 
 router.post('/editProduct/:id',imageUpload,async (req,res) =>{
   
-    /*let product = await Product.findById(req.params.id);
+  
+  Product.findById(req.params.id,async (err,doc)=>{
     var pic1 = "";
     var pic2 = "";
     var pic3 = "";
     var pic4 = "";
-    const fd = "product/" + product.productType + "/" +product.name;
-    
-    try{
-      await cloudinary.api.delete_resources(all, {folder : fd});
-    }catch(err){
-        console.log(err);
-    }
-    
+    var a = b = c = d =0;
+    const fd = "product/" + doc.productType + "/" +doc.name;
+    const newfd = "product/" + req.body.type + "/" + req.body.name;
+        
       
-      console.log(fd);
-      const fd2 = "product/" + req.body.type + "/" +req.body.name; 
-      console.log(fd2);
-      try{
-      pic1 = await cloudinary.uploader.upload(req.files.image1[0].path,{folder : fd2});
-      pic2 = await cloudinary.uploader.upload(req.files.image2[0].path,{folder : fd2});
-      pic3 = await cloudinary.uploader.upload(req.files.image3[0].path,{folder : fd2});
-      pic4 = await cloudinary.uploader.upload(req.files.image4[0].path,{folder : fd2});
+      for (var i in req.files){
+        if(req.files[i][0].fieldname == "image1"){
+          
+          await cloudinary.api.delete_resources(fd+"/p1");
+          
+          pic1 = await cloudinary.uploader.upload(req.files.image1[0].path,{folder : fd,public_id: "p1"});
+          pic1 = pic1.url;
+          a = 1;
+        }
+        else if (req.files[i][0].fieldname == "image2"){
+          
+          await cloudinary.api.delete_resources(fd+"/p2");
+         
+          pic2 = await cloudinary.uploader.upload(req.files.image2[0].path,{folder : fd,public_id: "p2"});
+          pic2 = pic2.url;
+          b = 1;
+        }
+        else if (req.files[i][0].fieldname == "image3"){
+          
+          await cloudinary.api.delete_resources(fd+"/p3");
+         
+          pic3 = await cloudinary.uploader.upload(req.files.image3[0].path,{folder : fd,public_id: "p3"});
+          pic3 = pic3.url;
+          c = 1;
+        }
+        else if (req.files[i][0].fieldname == "image4")
+        {
+          
+          await cloudinary.api.delete_resources(fd+"/p4");
+         
+          pic4 = await cloudinary.uploader.upload(req.files.image4[0].path,{folder : fd,public_id: "p4"});
+          d = 1;
+          pic4 = pic4.url;
+        }
       }
-      catch(err){
-        console.log(err);
+      if (a == 0){
+        pic1 = doc.images[0];
       }
-    
-      
-  */
-
-      
+      if (b == 0){
+        pic2 = doc.images[1];
+      }
+      if (c == 0){
+        pic3 = doc.images[2];
+      }
+      if (d == 0){
+        pic4 = doc.images[3];
+      }
  
-    Product.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, doc) => {
+      if(newfd != fd){
+        try{
+          pic1 = await cloudinary.uploader.rename(fd+"/p1",newfd+"/p1");
+          pic1 = pic1.url;
+          }catch(err){
+            console.log(err);
+          }
+          try{
+            pic2 = await cloudinary.uploader.rename(fd+"/p2",newfd+"/p2");
+            pic2 = pic2.url;
+            }catch(err){
+              console.log(err);
+            }
+            try{
+             pic3 = await cloudinary.uploader.rename(fd+"/p3",newfd+"/p3");
+             pic3 = pic3.url;
+              }catch(err){
+                console.log(err);
+              }
+              try{
+              pic4 =  await cloudinary.uploader.rename(fd+"/p4",newfd+"/p4");
+              pic4 = pic4.url;
+                }catch(err){
+                  console.log(err);
+                }
+        
+          await cloudinary.api.delete_resources(fd+"/p1");
+          
+        
+          await cloudinary.api.delete_resources(fd+"/p2");
+         
+        
+          await cloudinary.api.delete_resources(fd+"/p3");
+         
+        
+          await cloudinary.api.delete_resources(fd+"/p4");
+         
+          await cloudinary.api.delete_folder(fd);  
+                  
+      }
+
     
-    if (!err) { 
-      res.redirect("/products/id/" + req.body._id);
+          
+    var pic=[];
+    pic.push(pic1);
+    pic.push(pic2);
+    pic.push(pic3);
+    pic.push(pic4);
+    const data = {
+    productType : req.body.type,
+    name : req.body.name,
+    brand : req.body.brand,
+    price : req.body.price,
+    isSale : {
+        status : req.body.status,
+        percent : req.body.percent 
+    },
+    stock : req.body.stock,
+    screenSize : req.body.screenSize,
+    color : req.body.color,
+    features : req.body.features,
+    images : pic,
+    description : req.body.description,
+    detail : {
+        itemDimensions : req.body.itemDimensions,
+        itemWeight : req.body.itemWeight,
+        modelName : req.body.modelName,
+        processor : req.body.processor,
+        graphicsCard : req.body.graphicsCard,
+        os : req.body.os,
+        ram : req.body.ram,
+        storage : req.body.storage,
+        webcam : req.body.webcam,
+        battery : req.body.battery,
+        powerAdapter : req.body.powerAdapter,
+        manufacturer :req.body.manufacturer,
+        releaseDate : req.body.releaseDate,
+    },
+ 
     }
-    else {
-      console.log('Error during record update : ' + err);
-    }
-});
+    Product.findOneAndUpdate({ _id: req.body._id }, data, { new: true }, (err, doc) => {
+    
+      if (!err) { 
+        res.redirect("/products/id/" + req.body._id);
+      }
+      else {
+        console.log('Error during record update : ' + err);
+      }
+  });
+  });
+    
+    
   
   
 });
@@ -148,23 +274,26 @@ router.get('/editProduct/:id',(req,res)=>{
  router.get('/delete/:id', async (req,res)=>{
   Product.findById(req.params.id,async (err,doc)=>{
     const fd = "product/" + doc.productType + "/" +doc.name;
-    console.log(fd);
-    try{
+   
+    
+      
     await cloudinary.api.delete_resources(fd+"/p1");
+       
+       
     await cloudinary.api.delete_resources(fd+"/p2");
+         
+    
     await cloudinary.api.delete_resources(fd+"/p3");
+            
     await cloudinary.api.delete_resources(fd+"/p4");
-
+             
+    
     await cloudinary.api.delete_folder(fd);  
-    }catch(err){
-      console.log(err);
-    }       
+          
     
   });
-  
+
   Product.findByIdAndRemove(req.params.id,(err,doc)=>{
-    //await cloudinary.api.delete_resources([req.body.images[0],req.body.images[1],req.body.images[2],req.body.images[3]], {folder : fd});
-    
     if(!err)
         res.redirect('/products/1');
     
