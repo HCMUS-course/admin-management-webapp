d594573a8ad3f08580c97371185af51bf97fb6ab
19127324 <64062447+19127324@users.noreply.github.com>
2022-01-02 21:53:12 +0700

    Chart: top10product

--- components/product/orderController.js
+++ components/product/orderController.js
@@ -123,19 +123,80 @@ module.exports.chartByYear = async ()=>{
    temp.push(sale)
    return temp
 }
+module.exports.chartTop10 = async ()=>{
+    const order = await orderServices.findAllOrder()
+    const num = await orderServices.getNumberOfOrder()
+    var product = []
+    var sale = []
+    
+    for(let i = 0; i < num; i++){
+       order[i].items.forEach(element => {
+           var tmp = element.cart.items
+           tmp.forEach(e=>{
+            var t = e.name
+            var temp = t.concat('/')
+            var name = '/'.concat(temp)
+            
+            if(product.indexOf(name) == -1){
+                product.push(name)
+                sale.push(1)
+            }
+            else{
+                const j = product.indexOf(name)
+                sale[j] += 1
+            }
+           })
+           
+         
+       });
+        
+    }
+   var list = [];
+   for (var j = 0; j < product.length; j++) 
+   list.push({'product': product[j], 'sale': sale[j]});
 
+   list.sort(function(a, b) {
+   return ((a.sale > b.sale) ? -1 : ((a.product == b.product) ? 0 : 1));
+   });
+
+
+   for (var k = 0; k < list.length; k++) {
+    product[k] = list[k].product;
+    sale[k] = list[k].sale;
+   }
+  
+   var top10 = []
+   if(product.length<=10){
+      top10.push(product)
+   }
+   else{
+       var name = []
+       for( var i = 0 ; i<10;i++){  
+        name.push(product[i])
+       }
+       top10.push(name)
+   }
+   
+   top10.push(sale)
+   return top10
+}
 module.exports.draw = async (res) =>{
     const byDate = await orderController.chartByDate()
     const byMonth = await orderController.chartByMonth()
     const byYear = await orderController.chartByYear()
-    console.log(byYear)
+    const top10 = await orderController.chartTop10()
+    console.log(top10)
+   
+  
     res.render('Chart',{
         xValues : byDate[0],
         yValues : byDate[1],
         xValuesM : byMonth[0],
         yValuesM : byMonth[1],
         xValuesY : byYear[0],
-        yValuesY : byYear[1]
-    
+        yValuesY : byYear[1],
+        xtop10 : top10[0],
+        ytop10 : top10[1]
+        
     });
 }
